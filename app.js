87c3d77015d86010e3d158097582a85952c4f11d
Dmucci <dmucci93@gmail.com>
2023-11-09 11:57:54 +0100

    finished

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ createApp({
     const amount = ref("");
 
     const convertToDollars = computed(() => {
-      return amount.value; // TODO: Apartado 1. Modifica este return para realizar la conversión de euros a dolares
+      return amount.value*1.07; // TODO: Apartado 1. Modifica este return para realizar la conversión de euros a dolares
+    });
+
+    const convertToYen = computed(() => {
+      return amount.value*161.47;
     });
 
     return {
       amount,
       convertToDollars,
+      convertToYen,
     };
   },
 }).mount("#app");
